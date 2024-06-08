e2c72193c700f01b81a88b71b8d3cc399769ca76
Lucius Rick Dev <124920048+LuciusRick@users.noreply.github.com>
2024-06-07 21:15:22 -0300

    css ajustes

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,4 @@
-function mudouTamanho(){
-    if (window.innerWidth >= 768){
-        itensid.style.display = 'block'
-    } else {
-        itensid.style.display = 'none'
-    }
-}
+
 
 function clickMenu() {
     if (itensid.style.display == 'none'){
