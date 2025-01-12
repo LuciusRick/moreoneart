d1cf6efe6d5cbbd5f5ae9d853ef0d0794feed75f
Lucius Rick Dev <124920048+LuciusRick@users.noreply.github.com>
2025-01-12 14:54:41 -0300

    up

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,29 @@ function clickMenu() {
     }
     
 }
+
+    
+const track = document.querySelector('.carousel-track');
+    const slides = Array.from(track.children);
+    const nextButton = document.querySelector('.carousel-button.right');
+    const prevButton = document.querySelector('.carousel-button.left');
+    const slideWidth = slides[0].getBoundingClientRect().width;
+
+    let currentIndex = 0;
+
+    function moveToSlide(index) {
+      track.style.transform = `translateX(-${index * slideWidth}px)`;
+      currentIndex = index;
+    }
+
+    nextButton.addEventListener('click', () => {
+      if (currentIndex < slides.length - 1) {
+        moveToSlide(currentIndex + 1);
+      }
+    });
+
+    prevButton.addEventListener('click', () => {
+      if (currentIndex > 0) {
+        moveToSlide(currentIndex - 1);
+      }
+    });
